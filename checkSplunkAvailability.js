117666f32d169d386e4f8ed6e62d9334d687bbf2
NathanGr01 <nathan.gruber@gmx.de>
2024-03-01 09:03:35 +0100

    fix: Remove https setting for splunk connectivity check

--- checkSplunkAvailability.js
+++ checkSplunkAvailability.js
@@ -3,7 +3,6 @@ const splunkjs = require("splunk-sdk");
 const config = {
   username: "admin", // Update with actual admin username if different
   password: process.env.SPLUNK_PASSWORD, // Ensure SPLUNK_PASSWORD is set in your environment variables
-  scheme: "https",
   host: process.env.SPLUNK_CONTAINER_NAME, // Ensure SPLUNK_CONTAINER_NAME is set in your environment variables
   port: process.env.SPLUNK_MGMT_PORT, // Ensure SPLUNK_MGMT_PORT is set in your environment variables
 };
