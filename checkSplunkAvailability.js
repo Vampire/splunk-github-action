dbac5c109749f5eebaddb193e8e00867e30179ac
NathanGr01 <nathan.gruber@gmx.de>
2024-03-04 13:18:44 +0100

    fix: Fixing Splunk availability check

--- checkSplunkAvailability.js
+++ checkSplunkAvailability.js
@@ -15,15 +15,8 @@ console.log("Splunk management port:", process.env.SPLUNK_MGMT_PORT);
 
 const checkSplunkAvailability = async () => {
   try {
-    await service.login(function (err, success) {
-      if (err || !success) {
-        console.error("Splunk login failed:", err);
-        process.exit(2); // Exit with error code to indicate failure
-      } else {
-        console.log("Splunk login successful");
-        process.exit(0); // Exit with success code
-      }
-    });
+    const answer = await service.login();
+    console.log("Splunk is available:", answer);
   } catch (error) {
     console.error("Error occurred:", error);
     process.exit(2); // Exit with error code to indicate failure
